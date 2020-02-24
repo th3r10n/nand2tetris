22d5e463c0c6b33fcf826d036768bdb3148ceee7
Arturo Morales <am0rl@yahoo.com>
2020-02-23 23:59:36 -0300

    Committing tested multiplication assembly program.

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,30 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// M[0] = R0
+// M[1] = R1
+// M2[2] is initialized to 0
+
+@i
+M = 1 // i = 1
+@2 
+M = 0 // R2 = 0
+(LOOP)
+@i
+D = M // D = i
+@1
+D = M - D // R1 - i
+@END
+D;JLT
+@0
+D = M // D = R0
+@2
+M = M + D // R2 = R2 + R0
+@i
+M = M + 1 // i = i + 1
+@LOOP
+0;JMP // Goto LOOP
+(END)
+@END
+0;JMP
